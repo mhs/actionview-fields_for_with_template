3b66a50be15ad6a98c270ca857bcc27272d79f24
John Hwang <jhwang@mutuallyhuman.com>
2009-09-02 13:14:31 -0400

    fixed it to work with collections

--- lib/actionview-fields_for_with_template.rb
+++ lib/actionview-fields_for_with_template.rb
@@ -5,13 +5,20 @@ module ActionView
         options = args.extract_options!
         options[:child_index] = NumericSequence.new
         args << options
-        unless options[:template_only]
-          fields_for record_or_name_or_array, *args, &block
+        
+        records = object.send(record_or_name_or_array)
+        if records.any?
+          records.reject(&:new_record?).each do |record|
+            nargs = args.dup
+            nargs.unshift record
+            @template.fields_for "#{@object_name}[#{record_or_name_or_array.to_s.singularize}_attributes][#{record.id}]", record, *nargs, &block
+          end
         end
-        @template.fields_for "#{@object_name}[#{record_or_name_or_array.to_s.singularize}_attributes][new]", object.send(record_or_name_or_array).new, :index => "NEW_RECORD" do |f|
-          @template.concat %|<div class="template" style="display:none">|, @proc
+        
+        @template.fields_for "#{@object_name}[#{record_or_name_or_array.to_s.singularize}_attributes][new][NEW_RECORD]", object.send(record_or_name_or_array).new do |f|
+          @template.concat %|<div class="template" style="display:none">|, @proc.binding
           block.call f
-          @template.concat %|</div>|, @proc
+          @template.concat %|</div>|, @proc.binding
         end
       end
     end
