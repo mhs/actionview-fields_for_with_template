644eb7c8d59b9481954cc66e871de82b61d89f4c
Zach Church <zgchurch@gmail.com>
2010-08-06 13:10:01 -0400

    Use += instead of << for string concatenation

--- lib/actionview-fields_for_with_template.rb
+++ lib/actionview-fields_for_with_template.rb
@@ -7,7 +7,7 @@ module ActionView
         args << options
 
         result = fields_for record_or_name_or_array, *args, &block
-        result << fields_for record_or_name_or_array, object.send(record_or_name_or_array).new, :child_index => "NEW_RECORD" do |f|
+        result += fields_for record_or_name_or_array, object.send(record_or_name_or_array).new, :child_index => "NEW_RECORD" do |f|
           @template.concat %|<div class="template" style="display:none">|.html_safe
           block.call f
           @template.concat %|</div>|.html_safe
