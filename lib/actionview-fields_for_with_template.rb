8f74e29abb872bb7b543ffcb08cdc9c3c41195dd
Zach Church <zgchurch@gmail.com>
2010-08-06 11:10:03 -0400

    Update for Rails 3 RC

--- lib/actionview-fields_for_with_template.rb
+++ lib/actionview-fields_for_with_template.rb
@@ -5,12 +5,15 @@ module ActionView
         options = args.extract_options!
         options[:child_index] = NumericSequence.new
         args << options
-        fields_for record_or_name_or_array, *args, &block
-        fields_for record_or_name_or_array, object.send(record_or_name_or_array).new, :child_index => "NEW_RECORD" do |f|
-          @template.concat %|<div class="template" style="display:none">|
+
+        result = fields_for record_or_name_or_array, *args, &block
+        result << fields_for record_or_name_or_array, object.send(record_or_name_or_array).new, :child_index => "NEW_RECORD" do |f|
+          @template.concat %|<div class="template" style="display:none">|.html_safe
           block.call f
-          @template.concat %|</div>|
+          @template.concat %|</div>|.html_safe
         end
+
+        result
       end
     end
   end
